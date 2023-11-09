cc6b5437b6a01a1eadcfd4f7cc3c190e1627fef0
DMGamage <gamagemadhushan@gmail.com>
2023-11-09 09:40:54 +0530

    Finish password generator

---
+++ Password Generator/main.py
@@ -0,0 +1,45 @@
+import random
+import string
+
+def generate_password(min_length,numbers=True,special_character=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+
+    if numbers:
+        characters +=digits
+    if special_character:
+        characters += special
+
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special_character:
+            has_special = True
+
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_character:
+            meets_criteria = meets_criteria and has_special
+
+    return pwd
+
+min_length = int(input("enter minimum length: "))
+has_number_new = input("Do you want numbers include y or n ?: ").lower()=="y"
+has_special_new = input("Do you want numbers include y or n ?: ").lower()=="y"
+
+pwd=generate_password(min_length,has_number_new,has_special_new)
+print("Your generated password is : ",pwd)
+
+
